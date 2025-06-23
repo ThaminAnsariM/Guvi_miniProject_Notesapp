b66560a31ccd0f17a5c4a9be71e8dbc84a6b77ac
ThaminAnsariM <thameenansari456@gmail.com>
2025-06-23 18:25:00 +0530

    3rd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
-import Notecard from "./Components/Notecard";
-import TrashPage from "./Pages/TrashPage";
-import Nav from "./Components/Nav";
+import Notecard from "./src/Components/Notecard";
+import TrashPage from "./src/Pages/TrashPage";
+import Nav from "./src/Components/Nav";
 
 function App() {
   const [notes, setNotes] = useState([]);
